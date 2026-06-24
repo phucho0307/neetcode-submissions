e1ba7dc0a7f51ad2e0e21cf295e7603a193008ac
Phuc T Ho <158760870+phucho0307@users.noreply.github.com>
2026-06-24 12:44:37 -0500

    Add: eating-bananas - submission-16

---
+++ Data Structures & Algorithms/eating-bananas/submission-16.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        right = max(piles)
+        left = 1
+        while left<right:
+            mid = int(left + (right-left)/2)
+            k = 0
+            for b in piles:
+                k += b//mid if b%mid==0 else int(b/mid)+1
+            if k>h: left = mid+1
+            elif k<=h: right = mid
+            if left == right: return left
+        return mid
+
+
+
+
+        
